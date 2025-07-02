d975c18f3be8f93544f416a3b0a9d09205a376ea
AubainMbk <aubainmbokou1@gmail.com>
2025-07-02 05:31:18 +0200

    Update config_example.py

--- config_example.py
+++ config_example.py
@@ -1,1 +1,6 @@
+import streamlit as st
+KOBAN_GENERAL_API_KEY = st.secrets["API_KEY"]
+KOBAN_USER_API_KEY = st.secrets["ANOTHER_KEY"]
+KOBAN_ENDPOINT = "https://eu01.app-koban.com/api/v1/ncThird/PostOne?uniqueproperty=Email"
+CV_PATH = "data/CV_AUBAIN_MBOKOU.pdf"
 
